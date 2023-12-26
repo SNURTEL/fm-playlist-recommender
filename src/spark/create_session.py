f13cd0c7239faae0f517ad6b71a2a0cc627557d9
towienko <01169218@pw.edu.pl>
2023-12-26 20:45:16 +0100

    Use embeddings

--- src/spark/create_session.py
+++ src/spark/create_session.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 def create_session() -> SparkSession:
     spark = SparkSession.builder \
         .master("local[*]") \
-        .config("spark.driver.memory", "8g") \
+        .config("spark.driver.memory", "1g") \
         .getOrCreate()
 
     spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
