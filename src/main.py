20d504982efa43db1c883e1008b4764c8b18d1ac
Tomasz Owienko <tomek.owienko@gmail.com>
2024-01-11 14:07:14 +0100

    Fix startup

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, responses, Query
 from typing import List, Sequence
-from model.base_model import BaseModel
+
+from fastapi import FastAPI, responses, Query
 import pandas as pd
 import numpy as np
 from scipy import stats
-from spark.config import views
-from spark.create_session import create_session
+
+from src.model.base_model import BaseModel
+from src.spark.config import views
+from src.spark.create_session import create_session
 
 app = FastAPI()
 
-VIEWS = views("v4")
+VIEWS = views("v3")
 spark = create_session()
 
 for view, file in VIEWS.items():
@@ -19,7 +21,8 @@ for view, file in VIEWS.items():
 _tracks = spark.sql(
     f"SELECT DISTINCT id, id_artist, acousticness, danceability, duration_ms, energy, instrumentalness, key, liveness, loudness, popularity, EXTRACT(year from `release_date`) as release_year, speechiness, tempo, valence FROM tracks ").toPandas()
 users = spark.sql(f"SELECT user_id FROM users").toPandas()
-tracks = pd.concat([_tracks[['id', 'id_artist']], _tracks.drop(['id', 'id_artist'], axis=1).apply(stats.zscore)], axis=1)
+tracks = pd.concat([_tracks[['id', 'id_artist']], _tracks.drop(['id', 'id_artist'], axis=1).apply(stats.zscore)],
+                   axis=1)
 sessions = spark.sql(
     """
     SELECT s.user_id, s.track_id, s.weight, acousticness, danceability, duration_ms, energy, instrumentalness, key, liveness, loudness, popularity, EXTRACT(year from `release_date`) as release_year, speechiness, tempo, valence
@@ -42,6 +45,7 @@ base_model = BaseModel(
     sessions_df=sessions
 )
 
+
 @app.get("/", response_class=responses.PlainTextResponse)
 def root():
     return """IUM Task 3 Variant 2
@@ -59,5 +63,5 @@ def read_item(user_id: int, playlist_length: int, other_users: str = None):
     else:
         users = []
     users.insert(0, user_id)
-    result = base_model.predict_multiple(other_users, playlist_length)
+    result = base_model.predict_multiple(users, playlist_length)
     return result
